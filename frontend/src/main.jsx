5a502cdb4618982c7f6f1aabc71dd170a985e0a6
SanketJawali <135835658+SanketJawali@users.noreply.github.com>
2025-07-19 11:07:18 +0530

    added login and home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '@/index.css'
+import App from '@/App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from '@/components/layout';
+import Login from '@/components/login'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<Login />} />
+            </Route>
+
+        </Routes>
+    </BrowserRouter>
 )
