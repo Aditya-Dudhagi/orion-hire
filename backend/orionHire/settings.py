fb2b374d40ff5bf24fa45a74eff0170b2edf285e
Omkar2208 <omkarmangalpalli@gmail.com.com>
2025-07-21 23:06:19 +0530

    google auth

--- backend/orionHire/settings.py
+++ backend/orionHire/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 GOOGLE_CLIENT_ID = config("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = config("GOOGLE_CLIENT_SECRET")
+SECRET_KEY = config("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1!l*4hh^8htkxug!6#c58jrsahun%#$z&t2tzalci!dbygt^oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
