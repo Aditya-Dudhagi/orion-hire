7a75f10a14910bb4d997683fd91518ee46727fba
Omkar2208 <omkarmangalpalli@gmail.com.com>
2025-07-21 22:53:41 +0530

    google authentication done

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings # To access settings, including client ID
 
-# Create your views here.
+def login_page(request):
+   
+    google_client_id = settings.SOCIALACCOUNT_PROVIDERS['google']['APP']['client_id']
+
+    context = {
+        'google_client_id': google_client_id,
+    }
+    return render(request, 'login.html', context)
